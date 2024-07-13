97caf55cdf07062ea071e2d00153fb8a4e3c3c9e
Nam <nhhnam0405@gmail.com>
2024-07-13 14:32:36 +0700

    change device

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const path = require('path');
 const app = express();
 const port = 5000;
 const route = require('./routes/index');
+const cors = require('cors');
 
+//
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+//
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Thêm dòng này để parse JSON bodies
 route(app);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
